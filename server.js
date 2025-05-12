3c7211424ca0da3f2c3a56ac83d3f2afba8728ea
saanvi2233 <saanvigupta2233@gmail.com>
2025-05-12 13:10:47 +0530

    server initialized at port 5000

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+require('dotenv').config()  //to load the environmnet varibale
+
+app.use(cors()) //to allow cross-origin requests
+app.use(express.json()) //to parse json data
+
+app.get('/',(req,res)=>{
+    res.send('Hello from server!!')
+})
+
+const PORT=process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
